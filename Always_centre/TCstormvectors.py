b0dcb885593d8d0e4cae0947bde3d5e2f45ed4e5
Mohash76 <159174667+Mohash76@users.noreply.github.com>
2026-05-01 16:11:48 -0400

    Add files via upload

---
+++ Always_centre/TCstormvectors.py
@@ -0,0 +1,30 @@
+import numpy as np
+from TCcatalog_locations import *
+
+atl05vec = [atl05tc01,atl05tc02,atl05tc03,atl05tc04,atl05tc05,atl05tc06,atl05tc07,atl05tc08,atl05tc09,atl05tc10,atl05tc11,atl05tc12,atl05tc13,atl05tc14,atl05tc15,atl05tc16,atl05tc17]
+
+atl06vec = [atl06tc01,atl06tc02,atl06tc03,atl06tc04,atl06tc05,atl06tc06,atl06tc07,atl06tc08,atl06tc09,atl06tc10]
+
+epc05vec = [epc05tc01,epc05tc02,epc05tc03,epc05tc04,epc05tc05,epc05tc06,epc05tc07,epc05tc08]
+
+epc06vec = [epc06tc01,epc06tc02,epc06tc03,epc06tc04,epc06tc05,epc06tc06,epc06tc07,epc06tc08,epc06tc09,epc06tc10,epc06tc11,epc06tc12,epc06tc13,epc06tc14,epc06tc15,epc06tc16,epc06tc17,epc06tc18,epc06tc19]
+
+npc05vec = [npc05tc01,npc05tc02,npc05tc03,npc05tc04,npc05tc05,npc05tc06,npc05tc07,npc05tc08,npc05tc09,npc05tc10,npc05tc11,npc05tc12,npc05tc13,npc05tc14,npc05tc15,npc05tc16,npc05tc17,npc05tc18,npc05tc19,npc05tc20,npc05tc21,npc05tc22,npc05tc23]
+
+
+npc06vec = [npc06tc01,npc06tc02,npc06tc03,npc06tc04,npc06tc05,npc06tc06,npc06tc07,npc06tc08,npc06tc09,npc06tc10,npc06tc11,npc06tc12,npc06tc13,npc06tc14,npc06tc15,npc06tc16,npc06tc17,npc06tc18,npc06tc19,npc06tc20,npc06tc21]
+
+nio05vec = [nio05tc01,nio05tc02,nio05tc03,nio05tc04]
+
+nio06vec = [nio06tc01,nio06tc02,nio06tc03,nio06tc04,nio06tc05,nio06tc06]
+
+sio56vec = [sio56tc01,sio56tc02,sio56tc03,sio56tc04,sio56tc05,sio56tc06,sio56tc07,sio56tc08,sio56tc09,sio56tc10,sio56tc11,sio56tc12,sio56tc13,sio56tc14,sio56tc15,sio56tc16,sio56tc17,sio56tc18]
+
+sio67vec = [sio67tc01,sio67tc02,sio67tc03,sio67tc04,sio67tc05,sio67tc06,sio67tc07,sio67tc08,sio67tc09,sio67tc10,sio67tc11,sio67tc12]
+
+aus56vec = [aus56tc01,aus56tc02,aus56tc03,aus56tc04,aus56tc05,aus56tc06,aus56tc07,aus56tc08,aus56tc09,aus56tc10,aus56tc11,aus56tc12,aus56tc13,aus56tc14,aus56tc15,aus56tc16]#,aus56tc17]
+
+aus67vec = [aus67tc01,aus67tc02,aus67tc03,aus67tc04,aus67tc05,aus67tc06,aus67tc07,aus67tc08,aus67tc09,aus67tc10,aus67tc11,aus67tc12,aus67tc13,aus67tc14,aus67tc15,aus67tc16,aus67tc17]
+
+
+
